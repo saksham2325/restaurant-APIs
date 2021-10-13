5352fe9936c16c3fc6021a25f232cc931e9a9274
sakshamjtg <saksham.garg@joshtechnology.com>
2021-10-13 18:24:22 +0530

    readme file

---
+++ readme.md
@@ -0,0 +1,1 @@
+In this project i have create Rest APIs using Django Rest Framework.
--- restaurantinfo/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RestaurantinfoConfig(AppConfig):
-    name = 'restaurantinfo'
