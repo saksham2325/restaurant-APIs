e599185da8f5ab40c21f4688a2654bc332ddb487
sakshamjtg <saksham.garg@joshtechnologygroup.com>
2022-01-24 04:53:45 +0530

    self review

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,8 @@
-from rest_framework import authentication
-from rest_framework import permissions
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from verify_email.email_handler import send_verification_email
 
 from accounts import permissions as custompermission
 from accounts import serializers
